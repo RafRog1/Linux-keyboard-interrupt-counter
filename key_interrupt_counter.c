ae48c51e477ae663173cada54f61dfdfc6bb73ea
Rafał Rogalski <rogalski.ra@gmail.com>
2021-01-06 01:12:41 +0100

    Add main module file

---
+++ key_interrupt_counter.c
@@ -0,0 +1,15 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+static int __init key_interrupt_counter_init(void) {
+    printk(KERN_INFO "Key interrupt counter has beed loaded\n");
+    return 0;
+}
+
+static void __exit key_interrupt_counter_exit(void) {
+    printk(KERN_INFO "Key interrupt counter has beed unloaded\n");
+}
+
+module_init(key_interrupt_counter_init);
+module_exit(key_interrupt_counter_exit);
