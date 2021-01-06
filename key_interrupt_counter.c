64b1c6b4443e0ed43796b39371c7205b33277272
Rafał Rogalski <rogalski.ra@gmail.com>
2021-01-06 15:35:19 +0100

    Include headers

--- key_interrupt_counter.c
+++ key_interrupt_counter.c
@@ -1,6 +1,8 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/interrupt.h>
+#include <linux/ktime.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Rafal Rogalski");
