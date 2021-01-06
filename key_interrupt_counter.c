73b3a1687b82be5c5bccb81a0e3b5a103876f918
Rafał Rogalski <rogalski.ra@gmail.com>
2021-01-06 15:36:29 +0100

    Add function to count keyboard interrupts

--- key_interrupt_counter.c
+++ key_interrupt_counter.c
@@ -9,6 +9,15 @@ MODULE_AUTHOR("Rafal Rogalski");
 MODULE_DESCRIPTION("Module for counting interrupts from keyboard");
 MODULE_VERSION("0.1");
 
+static uint64_t count = 0;
+
+irq_handler_t irq_handler(int irq, void *dev_id, struct pt_regs *regs)
+{
+    count++;
+
+    return (irq_handler_t)IRQ_HANDLED;
+}
+
 static int __init key_interrupt_counter_init(void) 
 {
     printk(KERN_INFO "Key interrupt counter has beed loaded\n");
