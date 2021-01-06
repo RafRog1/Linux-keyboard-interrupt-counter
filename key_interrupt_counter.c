3a715b392f53e5764e22b217eccf71d936260817
Rafał Rogalski <rogalski.ra@gmail.com>
2021-01-06 18:59:54 +0100

    Change variable name

--- key_interrupt_counter.c
+++ key_interrupt_counter.c
@@ -12,7 +12,7 @@
 #define GET_COUT_TIME   _IOR('a', 'c', uint32_t*)
 
 static uint64_t count = 0;
-static int irq = 1, dev = 0;
+static int irq = 1, dev_irq = 0;
 static ktime_t time;
 
 static ssize_t read(struct file *file, char __user *buf, size_t len, loff_t *off);
@@ -41,14 +41,14 @@ static int __init key_interrupt_counter_init(void)
 {
     printk(KERN_INFO "Key interrupt counter has beed loaded\n");
     time = ktime_get_real();
-    return request_irq (irq, (irq_handler_t)irq_handler, IRQF_SHARED, "kbd_irq_handler", (void *)(&dev));
+    return request_irq (irq, (irq_handler_t)irq_handler, IRQF_SHARED, "kbd_irq_handler", (void *)(&dev_irq));
 }
 
 static void __exit key_interrupt_counter_exit(void) 
 {
     printk(KERN_INFO "Key interrupt counter has beed unloaded\n");
     synchronize_irq(irq);
-    free_irq(irq, &dev);
+    free_irq(irq, &dev_irq);
 }
 
 static ssize_t read(struct file *file, char __user *buf, size_t len, loff_t *off)
