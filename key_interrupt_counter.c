888c30f67537bf7c5d4029e1c6d58fe93601cc84
Rafał Rogalski <rogalski.ra@gmail.com>
2021-01-06 15:38:27 +0100

    Set request irq and free

--- key_interrupt_counter.c
+++ key_interrupt_counter.c
@@ -10,6 +10,7 @@ MODULE_DESCRIPTION("Module for counting interrupts from keyboard");
 MODULE_VERSION("0.1");
 
 static uint64_t count = 0;
+static int irq = 1, dev = 0;
 
 irq_handler_t irq_handler(int irq, void *dev_id, struct pt_regs *regs)
 {
@@ -21,12 +22,14 @@ irq_handler_t irq_handler(int irq, void *dev_id, struct pt_regs *regs)
 static int __init key_interrupt_counter_init(void) 
 {
     printk(KERN_INFO "Key interrupt counter has beed loaded\n");
-    return 0;
+    return request_irq (irq, (irq_handler_t)irq_handler, IRQF_SHARED, "kbd_irq_handler", (void *)(&dev));
 }
 
 static void __exit key_interrupt_counter_exit(void) 
 {
     printk(KERN_INFO "Key interrupt counter has beed unloaded\n");
+    synchronize_irq(irq);
+    free_irq(irq, &dev);
 }
 
 module_init(key_interrupt_counter_init);
