2bf290a6b85a01e3e459072bc2a3094716d5fcbc
Rafał Rogalski <rogalski.ra@gmail.com>
2021-01-06 18:56:24 +0100

    Add file operations functions

--- key_interrupt_counter.c
+++ key_interrupt_counter.c
@@ -3,6 +3,9 @@
 #include <linux/kernel.h>
 #include <linux/interrupt.h>
 #include <linux/ktime.h>
+#include <linux/ioctl.h>
+#include <linux/uaccess.h>
+#include <linux/fs.h>
 
 #define GET_COUNT       _IOR('a', 'a', uint64_t*)
 #define RESET_COUNT     _IO('a', 'b')
@@ -11,6 +14,21 @@
 static uint64_t count = 0;
 static int irq = 1, dev = 0;
 static ktime_t time;
+
+static ssize_t read(struct file *file, char __user *buf, size_t len, loff_t *off);
+static ssize_t write(struct file *file, const char __user *buf, size_t len, loff_t *off);
+static int open(struct inode *inode, struct file *file);
+static long ioctl(struct file *file, unsigned int cmd, unsigned long arg);
+static int release(struct inode *inode, struct file *file);
+
+static struct file_operations fops = {
+    .owner          = THIS_MODULE,
+    .read           = read,
+    .write          = write,
+    .open           = open,
+    .unlocked_ioctl = ioctl,
+    .release        = release,
+};
 
 irq_handler_t irq_handler(int irq, void *dev_id, struct pt_regs *regs)
 {
@@ -32,6 +50,48 @@ static void __exit key_interrupt_counter_exit(void)
     synchronize_irq(irq);
     free_irq(irq, &dev);
 }
+
+static ssize_t read(struct file *file, char __user *buf, size_t len, loff_t *off)
+{
+    printk(KERN_INFO "Read function\n");
+    return 0;
+}
+static ssize_t write(struct file *file, const char __user *buf, size_t len, loff_t *off)
+{
+    printk(KERN_INFO "Write function\n");
+    return 0;
+}
+static int open(struct inode *inode, struct file *file)
+{
+    printk(KERN_INFO "Device file open!\n");
+    return 0;
+}
+static int release(struct inode *inode, struct file *file)
+{
+    printk(KERN_INFO "Device file close!\n");
+    return 0;
+}
+static long ioctl(struct file *file, unsigned int cmd, unsigned long arg)
+{
+    uint32_t sec;
+    
+    switch(cmd){
+        case GET_COUNT:
+            copy_to_user((uint64_t *)arg, &count, sizeof(count));
+            break;
+        case RESET_COUNT:
+            count = 0;
+            time = ktime_get_real();
+            break;
+        case GET_COUT_TIME:
+            sec = time / (1000 * 1000 * 1000);
+            copy_to_user((uint32_t *)arg, &sec, sizeof(sec));
+        default:
+            break;
+    }
+    return 0;
+}
+
 
 module_init(key_interrupt_counter_init);
 module_exit(key_interrupt_counter_exit);
