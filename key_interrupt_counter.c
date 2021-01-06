b2d6cd64aa53986edf361278923f1a6daf25cc42
Rafał Rogalski <rogalski.ra@gmail.com>
2021-01-06 15:34:05 +0100

    Move function bracket to next line

--- key_interrupt_counter.c
+++ key_interrupt_counter.c
@@ -7,12 +7,14 @@ MODULE_AUTHOR("Rafal Rogalski");
 MODULE_DESCRIPTION("Module for counting interrupts from keyboard");
 MODULE_VERSION("0.1");
 
-static int __init key_interrupt_counter_init(void) {
+static int __init key_interrupt_counter_init(void) 
+{
     printk(KERN_INFO "Key interrupt counter has beed loaded\n");
     return 0;
 }
 
-static void __exit key_interrupt_counter_exit(void) {
+static void __exit key_interrupt_counter_exit(void) 
+{
     printk(KERN_INFO "Key interrupt counter has beed unloaded\n");
 }
 
